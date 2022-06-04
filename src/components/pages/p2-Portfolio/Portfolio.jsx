f5f42806f3f6abe555ecb9c83cb1842e0b36f28f
Leo <kovzel.leonid@mail.ru>
2022-06-04 20:17:26 +0300

    Added projects links

--- src/components/pages/p2-Portfolio/Portfolio.jsx
+++ src/components/pages/p2-Portfolio/Portfolio.jsx
@@ -4,19 +4,21 @@ import app from "../../../App.module.css";
 import {NavLink} from "react-router-dom";
 import {Title} from "../../5-common/Title/Title";
 import {Projects} from "./Projects/Projects";
+import close from '../../../assets/images/close-button.png'
+import {faSuitcase} from "@fortawesome/free-solid-svg-icons";
 
 const Portfolio = () => {
 	return (
 		<div className={`${s.portfolio}`}>
 			<div className={s.portfolioWrapper}>
 				<div className={`${s.portfolioInner} ${app.container}`}>
-					<Title title='my' accentTitle='portfolio' />
+					<Title title='my' accentTitle='portfolio' icon={faSuitcase} />
 					<div className={s.portfolioProjects}>
 						<Projects />
 					</div>
 				</div>
 			</div>
-			<NavLink to={'/CV'}  className={app.linkBack}><img src={''}/>X</NavLink>
+			<NavLink to={'/CV'}  className={app.linkBack}><img src={close}/></NavLink>
 		</div>
 	);
 };
--- src/components/pages/p2-Portfolio/Projects/Projects.jsx
+++ src/components/pages/p2-Portfolio/Projects/Projects.jsx
@@ -1,63 +1,38 @@
 import React from 'react';
 import s from './Projects.module.css';
-import js from '../../../../assets/images/js.svg'
+import {v1} from "uuid";
+import cover from "../Projects/ProjectCover/social.jpg"
+
+const projects = [
+	{id: v1(), heading: 'Todolist', demo: 'https://tomatovan.github.io/Todolist/', github: 'https://github.com/TomatoVan/Todolist', image: cover},
+	{id: v1(), heading: 'Social NetWork', demo: ' https://tomatovan.github.io/Social-Network/', github: 'https://github.com/TomatoVan/Social-Network', image: cover},
+	{id: v1(), heading: 'Interesting things', demo: 'https://tomatovan.github.io/Homeworks/', github: 'https://github.com/TomatoVan/Homeworks', image: cover},
+	{id: v1(), heading: 'Counter', demo: 'https://tomatovan.github.io/Counter_React/', github: 'https://github.com/TomatoVan/Counter_React', image: cover}
+]
 
 export const Projects = () => {
 
+	let projectsList = projects.map(proj => {
+		return (
+				<div className={s.tiles} key={proj.id} >
+					<div className={s.tile} >
+						<img src={proj.image} className={s.projectImage} alt={cover}/>
+						<div className={s.details}>
+							<span className={s.title}>{proj.heading}</span>
+							<span className={s.info}>
+								<a href={proj.demo} className={s.link} target="_blank">Demo</a>
+							</span>
+							<span className={s.info}>
+								<a href={proj.github} className={s.link} target="_blank">Github</a></span>
+						</div>
+					</div>
+				</div>
+		)
+	})
+
 	return (
 		<div className={s.projectBox}>
-			 <div className={s.tiles} >
-				<div className={s.tile} >
-					<img src={js} className={s.projectImage}/>
-					<div className={s.details}>
-						<span className={s.title}>title</span>
-						<span className={s.info}>
-								<a href={'#'} className={s.link} target="_blank">Demo</a>
-							</span>
-						<span className={s.info}>
-								<a href={'#'} className={s.link} target="_blank">Github</a></span>
-					</div>
-				</div>
-			</div>
-			<div className={s.tiles} >
-				<div className={s.tile} >
-					<img src={js} className={s.projectImage}/>
-					<div className={s.details}>
-						<span className={s.title}>title</span>
-						<span className={s.info}>
-								<a href={'#'} className={s.link} target="_blank">Demo</a>
-							</span>
-						<span className={s.info}>
-								<a href={'#'} className={s.link} target="_blank">Github</a></span>
-					</div>
-				</div>
-			</div>
-			<div className={s.tiles} >
-				<div className={s.tile} >
-					<img src={js} className={s.projectImage}/>
-					<div className={s.details}>
-						<span className={s.title}>title</span>
-						<span className={s.info}>
-								<a href={'#'} className={s.link} target="_blank">Demo</a>
-							</span>
-						<span className={s.info}>
-								<a href={'#'} className={s.link} target="_blank">Github</a></span>
-					</div>
-				</div>
-			</div>
-			<div className={s.tiles} >
-				<div className={s.tile} >
-					<img src={js} className={s.projectImage}/>
-					<div className={s.details}>
-						<span className={s.title}>title</span>
-						<span className={s.info}>
-								<a href={'#'} className={s.link} target="_blank">Demo</a>
-							</span>
-						<span className={s.info}>
-								<a href={'#'} className={s.link} target="_blank">Github</a></span>
-					</div>
-				</div>
-			</div>
+			{projectsList}
 		</div>
 	)
 }
