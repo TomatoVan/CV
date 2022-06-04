7ccb868ec2c82702e08f24b3900fd5e5d3714901
Leo <kovzel.leonid@mail.ru>
2022-06-04 18:59:07 +0300

    ContactsBlock refactor

--- src/components/pages/p3-Contacts/ContactBlock/ContactBlock.jsx
+++ src/components/pages/p3-Contacts/ContactBlock/ContactBlock.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import s from './ContactBlock.module.css';
 import {ContactFormBlock} from "./ContactFormBlock/ContactFormBlock";
+import {faMapMarkedAlt, faPhoneAlt} from '@fortawesome/free-solid-svg-icons';
+import {faGithub, faTelegramPlane} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
+
+const contact = [
+	{title: 'phone', text: '+7(980)-707-20-83', icon: faPhoneAlt},
+	{title: 'telegram', text: '@LeonKovz', icon: faTelegramPlane},
+	{title: 'github', text: 'To Github', icon: faGithub},
+	{title: 'address', text: 'Yaroslavl, Russia', icon: faMapMarkedAlt},
+]
 
 export const ContactBlock = () => {
+
+	let contactsBlock = contact.map((contact, index) => {
+		return (
+				<li  className={s.contactItem} key={index}>
+					<h3 className={s.contactTitle}>{contact.title}</h3>
+					<span className={s.contactIcon}><FontAwesomeIcon icon={contact.icon}/></span>
+					{contact.text === 'To Github' ?
+							<a className={s.contactLink} target='_blank' href='https://github.com/TomatoVan'>{contact.text}</a> :
+							<span className={s.contactText}>{contact.text}</span>
+					}
+				</li>
+		)
+	})
 
 	return (
 		<div className={s.contactBox}>
 			<ul className={s.contactList}>
-				 <li  className={s.contactItem}>
-				<h3 className={s.contactTitle}>title</h3>
-				<span className={s.contactIcon}>*icon*</span>
-					<span className={s.contactText}>description</span>
-				</li>
-				<li  className={s.contactItem}>
-					<h3 className={s.contactTitle}>title</h3>
-					<span className={s.contactIcon}>*icon*</span>
-					<span className={s.contactText}>description</span>
-				</li>
-				<li  className={s.contactItem}>
-					<h3 className={s.contactTitle}>title</h3>
-					<span className={s.contactIcon}>*icon*</span>
-					<span className={s.contactText}>description</span>
-				</li>
-				<li  className={s.contactItem}>
-					<h3 className={s.contactTitle}>title</h3>
-					<span className={s.contactIcon}>*icon*</span>
-					<span className={s.contactText}>description</span>
-				</li>
+				{contactsBlock}
 			</ul>
 
 			<ContactFormBlock />
--- src/components/pages/p3-Contacts/Contacts.jsx
+++ src/components/pages/p3-Contacts/Contacts.jsx
@@ -4,23 +4,23 @@ import {Title} from "../../5-common/Title/Title";
 import app from "../../../App.module.css";
 import {NavLink} from "react-router-dom";
 import {ContactBlock} from "./ContactBlock/ContactBlock";
+import {faPhoneAlt} from '@fortawesome/free-solid-svg-icons';
+import close from "../../../assets/images/close-button.png";
 
 const Contacts = () => {
 	return (
 		<div className={`${s.contacts} ${s.animation}`}>
 			<div className={s.contactsWrapper}>
 				<div className={`${s.contactsInner} ${app.container}`}>
-					<Title title='my' accentTitle='contacts' />
+					<Title title='my' accentTitle='contacts' icon={faPhoneAlt} />
 				</div>
-
-
 				<div>
 					<ContactBlock />
 				</div>
 
 			</div>
 
-			<NavLink to={'/CV'}  className={app.linkBack}><img src={''}/>X</NavLink>
+			<NavLink to={'/CV'}  className={app.linkBack}><img src={close}/></NavLink>
 		</div>
 	);
 };
