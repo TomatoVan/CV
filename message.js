eef67db2968608731a8571461b74e415e2fb0a04
Leonid Kovzel <kovzel.leo@gmail.com>
2022-09-10 17:55:23 +0300

    feat: add Procfile for heroku

--- message.js
+++ message.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
@@ -5,7 +7,28 @@ const app = express()
 // create reusable transporter object using the default SMTP transport
 const nodemailer = require('nodemailer')
 
+let port = process.env.PORT || 3000
+let smtp_login = process.env.SMTP_LOGIN || '---'
+let smtp_password = process.env.SMTP_PASSWORD || '---'
+
 app.use(cors())
+
+//testing
+app.use(express.json())
+// Your static pre-build assets folder
+app.use(express.static(path.join(__dirname, '..', 'build')))
+// Root Redirects to pre-build assets
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build'))
+})
+// Any Page Redirects to pre-build assets folder index.html that // will load the React app
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening!, ${port}`)
+})
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,24 +36,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-let port = process.env.PORT || 3000
-let smtp_login = process.env.SMTP_LOGIN || '---'
-let smtp_password = process.env.SMTP_PASSWORD || '---'
-
-app.listen(port, () => {
-  console.log(`Example app listening!, ${port}`)
-})
-
 let transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
     user: smtp_login, // generated ethereal user
     pass: smtp_password, // generated ethereal password
   },
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
 })
 
 app.post('/sendMessage', async (req, res) => {
