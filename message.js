3aca6461dd7f02c216a89888c736c15c21e4b845
Leonid Kovzel <kovzel.leo@gmail.com>
2022-09-10 17:19:31 +0300

    feat: add heroku credentials

---
+++ message.js
@@ -0,0 +1,49 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const app = express()
+// create reusable transporter object using the default SMTP transport
+const nodemailer = require('nodemailer')
+
+app.use(cors())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+let port = process.env.PORT || 3000
+let smtp_login = process.env.SMTP_LOGIN || '---'
+let smtp_password = process.env.SMTP_PASSWORD || '---'
+
+app.listen(port, () => {
+  console.log(`Example app listening!, ${port}`)
+})
+
+let transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: smtp_login, // generated ethereal user
+    pass: smtp_password, // generated ethereal password
+  },
+})
+
+app.post('/sendMessage', async (req, res) => {
+  let { name, email, message } = req.body.data
+
+  console.log(res.body)
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `"${name}" <${email}>`, // sender address
+    to: '<tomatovan13@gmail.com>', // list of receivers
+    subject: 'Message from CV form ✔', // Subject line
+    html: `<b>Letter from CV:</b>
+	<div>name: ${name}</div>
+	<div>contacts: ${email}</div>
+	<div>message: ${message}</div>`,
+  })
+
+  console.log(info)
+})
